95a8f7b1f90c8d65e41ac9224ca0b3c0099e81c6
Jordan <jiadonghe007@outlook.com>
2025-09-12 15:31:07 +0800

    refactor: modular ui design

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
     let cfg = slint_build::CompilerConfiguration::new().with_style("native".into());
-    slint_build::compile_with_config("ui.slint", cfg).expect("slint build failed");
+    slint_build::compile_with_config("ui/app.slint", cfg).expect("slint build failed");
 }
