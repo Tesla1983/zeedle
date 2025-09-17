60baf10c8810ae9b1794c8592dc95b73bacb4c57
Jordan <jiadonghe007@outlook.com>
2025-09-17 21:54:57 +0800

    chore: only build dark version ui

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let cfg = slint_build::CompilerConfiguration::new().with_style("native".into());
+    let cfg = slint_build::CompilerConfiguration::new().with_style("fluent-dark".into());
     slint_build::compile_with_config("ui/app.slint", cfg).expect("slint build failed");
     if std::env::var("CARGO_CFG_TARGET_OS").expect("can't find this env variable!") == "windows" {
         let mut res = winresource::WindowsResource::new();
