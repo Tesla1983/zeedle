45f9d6e1662dd75a55ad869a4433b23db59295e2
Jordan <jiadonghe007@outlook.com>
2025-09-27 23:44:01 +0800

    tutorial: add rodio usage demo

---
+++ examples/rodio_usage.rs
@@ -0,0 +1,28 @@
+use std::{thread, time::Duration};
+
+use rodio::Decoder;
+fn main() {
+    // create an output stream
+    let stream_handle = rodio::OutputStreamBuilder::from_default_device()
+        .expect("no output device available")
+        .open_stream()
+        .expect("failed to open output stream");
+    // create a sink to play audio
+    let sink = rodio::Sink::connect_new(&stream_handle.mixer());
+    // open an audio file
+    let file = std::fs::File::open("audios/爱情转移.flac").expect("failed to open audio file");
+    // decode the audio file
+    let source = Decoder::try_from(file).expect("failed to decode audio file");
+    // append the audio source to the sink & auto play
+    sink.append(source);
+    // sleep for a while to let the audio play
+    thread::sleep(Duration::from_secs(20));
+    // pause the audio playback explicitly
+    sink.pause();
+    // sleep for a while
+    thread::sleep(Duration::from_secs(20));
+    // resume the audio playback explicitly
+    sink.play();
+    // keep the main thread alive while the audio is playing
+    thread::sleep(Duration::from_secs(20));
+}
